875850e050b1399c74e13f8d0764a54fdd953d5c
zalik198 <zshomakhov@mail.ru>
2020-08-03 07:06:02 +0300

    CustomTableViewCell

---
+++ birthdayFriends/Controller/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  birthdayFriends
+//
+//  Created by Shom on 03.08.2020.
+//  Copyright © 2020 Shom. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imagePerson: UIImageView!
+    @IBOutlet weak var namePerson: UILabel!
+    @IBOutlet weak var statusPerson: UILabel!
+    @IBOutlet weak var dateBirthday: UILabel!
+    
+}
--- birthdayFriends/Controller/TableViewController.swift
+++ birthdayFriends/Controller/TableViewController.swift
@@ -27,11 +27,11 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = persons[indexPath.row]
-        cell.imageView?.image = UIImage(named: imagePerson[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 3
-        cell.imageView?.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        cell.namePerson.text = persons[indexPath.row]
+        cell.imagePerson.image = UIImage(named: imagePerson[indexPath.row])
+        cell.imagePerson.layer.cornerRadius = cell.imagePerson.frame.size.height / 3
+        cell.imagePerson.clipsToBounds = true
 
         return cell
     }
