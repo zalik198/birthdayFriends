5da998f635d65bf4e31c2948b61a530bf583dfb3
zalik198 <zshomakhov@mail.ru>
2020-08-07 01:04:10 +0300

    design app

--- birthdayFriends/Controller/TableViewController.swift
+++ birthdayFriends/Controller/TableViewController.swift
@@ -18,6 +18,10 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.backgroundView = UIImageView(image: UIImage(named: "back.png"))
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "back.png"), for: .default)
+        
+
 
     }
 
@@ -30,11 +34,19 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         cell.namePerson.text = person[indexPath.row].name
+        cell.namePerson.font = .boldSystemFont(ofSize: 19)
         cell.statusPerson.text = person[indexPath.row].status
         cell.dateBirthday.text = person[indexPath.row].dateBirthday
         cell.imagePerson.image = UIImage(named: person[indexPath.row].image)
         cell.imagePerson.layer.cornerRadius = cell.imagePerson.frame.height / 3
         cell.imagePerson.clipsToBounds = true
+        
+        
+        //fon cell
+        //cell.backgroundColor = UIColor(patternImage: UIImage(named: "back.png")!)
+        cell.backgroundColor = .clear
+
+        
 
         return cell
     }
